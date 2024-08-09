00bd6391e396f0350425f6ba6b9f7e59b67794f4
Aidshiv <163630330+Aidshiv@users.noreply.github.com>
2024-08-09 10:03:03 +0530

    floyd&warshall.c

--- floyd&warshall.c
+++ floyd&warshall.c
@@ -1,105 +1,62 @@
 #include <stdio.h>
-void floyd(int n, int cam[20][20]);
-void printmat(int dist[20][20], int n);
-void readmat();
-void main() {
-    readmat();
+int min(int a, int b)
+{
+    return (a < b ? a : b);
 }
-void readmat() {
-    int cam[20][20], n, j, i;
-    printf("Enter the number of vertices: ");
-    scanf("%d", &n);
-    printf("Enter the cost adjacency matrix:\n");
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            scanf("%d", &cam[i][j]);
-        }
-    }
-    floyd(n, cam);
+void floyd(int D[][10], int n)
+{
+    for (int k = 1; k <= n; k++)
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= n; j++)
+                D[i][j] = min(D[i][j], D[i][k] + D[k][j]);
 }
-void floyd(int n, int cam[20][20]) {
-    int i, k, j, dist[20][20];
 
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            dist[i][j] = cam[i][j];
-        }
-    }
-    for (k = 0; k < n; k++) {
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < n; j++) {
-                if (dist[i][j] > dist[i][k] + dist[k][j]) {
-                    dist[i][j] = dist[i][k] + dist[k][j];
-                }
-            }
-        }
-    }
-    printmat(dist, n);
-}
-void printmat(int dist[20][20], int n) {
-    int i, j;
-    printf("Matrix containing shortest distance between two vertices is:\n");
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            printf("%d\t", dist[i][j]);
-        }
+int main()
+{
+    int n, cost[10][10];
+    printf("Enter no. of Vertices: ");
+    scanf("%d", &n);
+    printf("Enter the cost matrix\n");
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+            scanf("%d", &cost[i][j]);
+    floyd(cost, n);
+
+    printf("All pair shortest path\n");
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+            printf("%d ", cost[i][j]);
         printf("\n");
     }
 }
 
+/***************************warshall*************/
+#include <stdio.h>
+void warshal(int A[][10], int n)
+{
+    for (int k = 1; k <= n; k++)
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= n; j++)
+                A[i][j] = A[i][j] || (A[i][k] && A[k][j]);
+}
 
+void main()
+{
+    int n, adj[10][10];
+    printf("Enter no. of Vertices: ");
+    scanf("%d", &n);
+    printf("Enter the adjacency matrix\n");
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+            scanf("%d", &adj[i][j]);
+    warshal(adj, n);
 
-#*************** WarShall ******************
-    #include <stdio.h>
-
-void wrshl(int n, int a[20][20]) {
-    int i, j, k, m1[20][20], m2[20][20];
-
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            m1[i][j] = a[i][j];
-        }
-    }
-
-    for (k = 0; k < n; k++) {
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < n; j++) {
-                m2[i][j] = (m1[i][j] || (m1[i][k] && m1[k][j]));
-            }
-        }
-
-        for (i = 0; i < n; i++) {
-                       for (j = 0; j < n; j++) {
-                m1[i][j] = m2[i][j];
-            }
-        }
-    }
-
-    printf("Transitive closure of the given matrix is:\n");
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            printf("%d\t", m1[i][j]);
-        }
+    printf("Transitive closure of the given graph is\n");
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+            printf("%d ", adj[i][j]);
         printf("\n");
     }
-}
-
-void readmat() {
-    int a[20][20], n, i, j;
-
-    printf("Enter the number of vertices: ");
-    scanf("%d", &n);
-
-    printf("Enter the adjacency matrix:\n");
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            scanf("%d", &a[i][j]);
-        }
-    }
-
-    wrshl(n, a);
-}
-
-void main() {
-    readmat();
 }
