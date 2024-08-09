3458c654ff9152c7492aa90a319ce0297cfd9f14
Aidshiv <163630330+Aidshiv@users.noreply.github.com>
2024-08-09 10:50:22 +0530

    output.c

---
+++ output.c
@@ -0,0 +1,110 @@
+*****kruskal*****
+Enter the no. of vertices:4
+Enter the cost matrix
+999 2 3 1
+2 999 4 999
+3 4 999 5
+1 999 5 999
+the minimum spanning tree edges are...
+From vertex 0 to vertex 3 and the cost = 1
+From vertex 0 to vertex 1 and the cost = 2
+From vertex 0 to vertex 2 and the cost = 3
+Cost of MST = 6
+
+*****prims*****
+Enter the no. of vertices: 4
+Enter the cost matrix
+999 2 3 1
+2 999 4 999
+3 4 999 5
+1 999 5 999
+Edge from vertex 0 to vertex 3 and the cost 1
+Edge from vertex 0 to vertex 1 and the cost 2
+Edge from vertex 0 to vertex 2 and the cost 3
+minimum spanning tree cost is 6
+*****floyd*****
+Enter no. of Vertices: 4
+Enter the cost matrix
+999 8 4 999
+999 999 1 999
+Page No. 13
+4 999 999 999
+999 2 9 999
+All pair shortest path
+8 8 4 999
+5 13 1 999
+4 12 8 999
+7 2 3 99
+*****warshall*****
+Enter no. of Vertices: 4
+Enter the adjacency matrix
+0 1 0 0
+0 0 0 1
+0 0 0 0
+1 0 1 0
+Transitive closure of the given graph is
+1 1 1 1
+1 1 1 1
+0 0 0 0
+1 1 1 1
+
+*****dijkstra*****
+Enter the no.of vertices:5
+Enter the cost matrix
+0 3 1 999 999
+3 0 7 5 1
+1 7 0 2 999
+999 5 2 0 7
+999 1 999 7 0
+Enter the source vertex:0
+the shortest distance is...Cost from 0 to 0 is 0
+Cost from 0 to 1 is 3
+Cost from 0 to 2 is 1
+Cost from 0 to 3 is 3
+Cost from 0 to 4 is 4
+
+*****topological*****
+Enter no. of Vertices: 5
+Enter the cost matrix
+0 1 1 0 0
+0 0 0 1 1
+0 0 0 1 0
+0 0 0 0 1
+0 0 0 0 0
+Topological ordering is:0 1 2 3 4
+
+****knap dynamic*****
+Enter the number of objects: 3
+Enter the knapsack capacity: 10
+Enter profit followed by weight:
+2 3 9
+5 3 6
+Max profit = 11
+
+*****knap greedy*****
+Enter the number of items: 3
+Enter the weight of the knapsack: 12
+Enter profit and weight for item 1: 4 5 2
+Enter profit and weight for item 2: 2 7 3
+Enter profit and weight for item 3: Choose the type of knapsack problem:
+1. Discrete Knapsack Problem
+2. Continuous Knapsack Problem
+1
+Maximum value in discrete knapsack = 13
+*****sum of subset*****
+enter no of elements:5
+enter the elements in increasing order:1 2 3 4 5
+eneter the sum:10
+subset = 1
+Page No. 24
+1 2 3 4
+subset = 2
+1 4 5
+subset = 3
+2 3 5
+
+*****selectionsort *****
+take differen of 5000 
+*****n queen *****
+
+no of queens : 3
